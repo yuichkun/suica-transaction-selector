98fe52d21b2ed44e4e97ea419909f2aa744403ae
yuichkun <yuichkun@gmail.com>
2024-01-02 20:52:02 +0900

    enable tailwind

--- popup.tsx
+++ popup.tsx
@@ -1,24 +1,12 @@
 import { useState } from "react"
+import './style.css'
 
 function IndexPopup() {
   const [data, setData] = useState("")
 
   return (
-    <div
-      style={{
-        padding: 16
-      }}>
-      <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
-      </h2>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <a href="https://docs.plasmo.com" target="_blank">
-        View Docs
-      </a>
+    <div className="w-[300px] h-[800px]">
+      <h1 className="text-lg">Suica 交通費計算ヘルパー</h1>
     </div>
   )
 }
